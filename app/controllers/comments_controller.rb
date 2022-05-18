0d45c7acc39be99e7976e5d5d46c24bf0860b173
JanTigeros <jan.sajtl@gmail.com>
2022-05-18 14:33:29 +0200

    make it better

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,17 @@
+# module Authors
+    class CommentsController < ApplicationController
+
+        def create
+            @post = Post.find(params[:post_id])
+            @comment = @post.comments.create(params[:comment].permit(:name, :body))
+            #@comment = @post.comments.create(params[:comment])
+            redirect_to post_path(@post)
+        end
+
+        
+
+
+    end
+
+
+# end
---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  devise_for :authors
+  get 'home/index'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root to: "home#index"
+  
+  scope module: 'authors' do
+    resources :posts do
+      resources :elements
+    end
+  end
+  resources :posts do
+    resources :comments
+  end
+end
